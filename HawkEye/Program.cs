e2ea55527a189813981b3563a062a1f671e9bd66
ye3967585 <864567293@qq.com>
2018-04-05 01:41:59 +0800

    img

--- HawkEye/Program.cs
+++ HawkEye/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using LevelManager;
+using HawkEngine.IO;
+using HawkEngine.Edit;
 
 namespace HawkEye
 {
@@ -11,8 +13,25 @@ namespace HawkEye
     {
         static void Main(string[] args)
         {
-            Level game = new Level();
-            game.Level0();
+            Text text = new Text();
+            ProgressBar progressBar = new ProgressBar();
+            FormColum formColum = new FormColum();
+            FILE f = new FILE();
+            Console.ReadKey(true);
+            Console.CursorVisible = false;
+            Console.WriteLine();
+            formColum.ShowSimpleFormColumn("  New Form", 30,15);
+            Console.WriteLine("\n  1.Button");
+            Console.WriteLine("  2.Form");
+            Console.WriteLine("  3.Exit\n\n");
+            Console.ReadKey(true);
+            formColum.ShowRichFormColumn("   Dir", 30, 15, "  ID    ", "Name     ");
+            string[] Index = f.GetDirIndex("Game\\");
+            for (int i = 0; i < Index.Length; i++)
+            {
+                text.OutPutText("  "+ i + "         " + Index[i]+"\n",15);
+            }
+            Console.ReadKey(true);
         }
     }
 }
